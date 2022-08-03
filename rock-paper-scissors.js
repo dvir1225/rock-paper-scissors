8dc72b20286366b7a9f9a52407d7bc917dd71fc5
Dvir <dvir1225@gmail.com>
2022-08-03 21:35:41 +0300

    call getPlayerChoice() and getComputerChoice()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,6 +3,7 @@ let computerSelection
 let playerInput = prompt("rock, paper or scissors?");
 let playerSelection
 
+/* places player input into variable*/
 function getPlayerChoice (){
     playerSelection = playerInput.toLowerCase()
     if ((playerSelection!=="rock") && (playerSelection!=="paper") && (playerSelection!=="scissors")){
@@ -11,54 +12,67 @@ function getPlayerChoice (){
         return playerSelection;
     }
 }
+getPlayerChoice();
 
+console.log(playerSelection);
+
+/*randomizes computer selection*/
 function getComputerChoice (){
     computerSelection = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     return computerSelection;
-        }
-
-
-function playRound (computerSelection, playerSelection){
-if (computerSelection == playerSelection){
-    return "it's a draw! try again"
-    } else if (computerSelection == "rock" && playerSelection == "paper"){
-      return "paper beats rock! you win!"
-    } else if (computerSelection == "rock" && playerSelection == "scissors"){
-     return "rock beats scissors! you lose!"
-    }  else if (computerSelection == "paper" && playerSelection == "scissors"){
-       return "scissors beat paper! you win!"
-    } else if (computerSelection == "paper" && playerSelection == "rock"){
-       return "paper beats rock! you lose!"
-    } else if (computerSelection == "scissors" && playerSelection == "rock"){
-       return "rock beats scissors! you win!"
-    } else if (computerSelection == "scissors" && playerSelection == "paper"){
-      return "scissors beat paper! you lose!"
-      }
 }
+getComputerChoice();
 
-let playerScore=0;
-let computerScore=0;
-
+console.log(computerSelection);
+        
+/*takes computer and user selection and declares winner of one round*/
+function playRound (computerSelection, playerSelection){
+    if(computerSelection == "paper" && playerSelection == "paper"){
+    return "its a draw!"
+    } else if(computerSelection == "rock" && playerSelection == "rock"){
+        return "its a draw!"
+    } else if(computerSelection == "scissors" && playerSelection == "scissors"){
+        return "its a draw!"
+    } else if(computerSelection == "rock" && playerSelection == "paper"){
+      return "paper beats rock! you win!"
+    } else if(computerSelection == "rock" && playerSelection == "scissors"){
+     return "rock beats scissors! you lose!"
+    }  else if(computerSelection == "paper" && playerSelection == "scissors"){
+       return "scissors beat paper! you win!"
+    } else if(computerSelection == "paper" && playerSelection == "rock"){
+       return "paper beats rock! you lose!"
+    } else if(computerSelection == "scissors" && playerSelection == "rock"){
+       return "rock beats scissors! you win!"
+    } else if(computerSelection == "scissors" && playerSelection == "paper"){
+      return "scissors beat paper! you lose!"
+      } 
+}
+playRound(computerSelection, playerSelection);
+console.log(playRound(computerSelection, playerSelection));
+/*places result of round into variable*/
 let roundResult = playRound(computerSelection, playerSelection);
 console.log(roundResult)
-/*
 
-getComputerChoice();
-getPlayerChoice();
+/*declare default user and computer scores*/
+let playerScore = 0;
+let computerScore = 0;
 
-function game ()  {
-    
-    for (let i=0; i < 5; i++){
-        playRound()
-            if (roundResult.includes("win")){
-            playerScore = ++playerScore;
-        } else if (roundResult.includes("lose")) {
-             computerScore = ++computerScore;
-        } else {
-            playerScore = playerScore;
-            computerScore = computerScore ;
-        }  
-    }   console.log('players score is ' + playerScore + ' and computer score is ' + computerScore)
+/*makes a game of 5*/
+function game(computerSelection, playerSelection, playerScore, computerScore) {
+    let intPlayerScore = parseInt(playerScore);
+    let intComputerScore = parseInt(computerScore);
+                for (i=0; i<5; i++) {
+        playRound(computerSelection, playerSelection);
+        console.log("player score is " + intPlayerScore);
+        console.log("computer score is " + intComputerScore);
+        break;
+        if (roundResult.includes("win")) {
+            intPlayerScore = ++intPlayerScore;
+        } if(roundResult.includes("lose")) {
+            intComputerScore = ++intComputerScore;
+        } else {}
+    }
 }
 
-game();*/
+gameScore = game(computerSelection, playerSelection, playerScore, computerScore);
+console.log(gameScore);
